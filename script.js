55ba39ed5434974c17094a4184daa50d877ab628
1037-NaniNaidu <kajadileep6112@gmail.com>
2026-08-21 14:44:03 +0530

    Update alert messages and add event listener

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-alert("SCRIPT.JS TEST 123");    
+alert("New script test 123");
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const button = document.getElementById("addCustomer");
+
+    if (button) {
+        alert("ADD CUSTOMER BUTTON FOUND");
+
+        button.addEventListener("click", function () {
+            alert("ADD CUSTOMER CLICKED");
+        });
+
+    } else {
+        alert("ADD CUSTOMER BUTTON NOT FOUND");
+    }
+
+});  
         
