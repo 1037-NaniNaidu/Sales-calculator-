3b35c95fded2581a8208cb23eec56a2b88cd614e
1037-NaniNaidu <kajadileep6112@gmail.com>
2026-08-21 14:18:39 +0530

    Add event listener for add customer button

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 alert("SCRIPT.JS WORKING");
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const addCustomerButton =
+        document.getElementById("addCustomer");
+
+    addCustomerButton.addEventListener("click", function () {
+        alert("ADD CUSTOMER WORKING");
+    });
+
+});
